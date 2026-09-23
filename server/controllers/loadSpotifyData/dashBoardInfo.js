9839de9f3320f2321a2c835b8dac3496899e9ccc
Szymon Zaremba <stary5717@gmail.com>
2026-09-23 19:36:54 +0200

    beginning of dashboard

--- server/controllers/loadSpotifyData/dashBoardInfo.js
+++ server/controllers/loadSpotifyData/dashBoardInfo.js
@@ -1,16 +1,13 @@
 import {throwError} from "../../utlils/errorManager.js";
+import {spotifyFetchSchema} from "../../utlils/spotifyFetchSchema.js";
 
 class dashboard{
     async #getUserProfile(accessToken){
         const url = "https://api.spotify.com/v1/me";
-        const headers = {
-            headers: {
-                Authorization: 'Bearer ' + accessToken
-            }
-        }
+
         const response = await fetch(url, {
             method: "GET",
-            headers: headers
+            headers: spotifyFetchSchema.basicHeader(accessToken)
         });
 
         if(!response.ok) throwError("problem with spotify api" , 500);
@@ -22,9 +19,63 @@ class dashboard{
         return [account_id, display_name, images];
     }
 
-    async #getTopArtists(accessToken){
+    async #getTopItems(accessToken , type){
+        const url = "https://api.spotify.com/v1/me/top/";
+        const timeRanges = ["short_term" , "medium_term" , "long_term"];
+        const limit = 5;
+        const offset = 0;
+        const fetches = new Array(3);
 
+        for(let i = 0; i<= 2; i++) {
+            fetches[i] = fetch(
+                url
+                + type
+                + "?time_range=" + timeRanges[i]
+                + "&limit=" + limit
+                + "&offset=" + offset
+            , {
+                    headers: spotifyFetchSchema.basicHeader(accessToken)
+                })
+        }
+        const [shortTerm , mediumTerm , longTerm]  = await Promise.all(fetches);
+
+        if(shortTerm.ok && mediumTerm.ok && longTerm.ok){
+            const shortTermData = await shortTerm.json();
+            const mediumTermData = await mediumTerm.json();
+            const longTermData = await longTerm.json();
+
+        }
     }
+
+     #getCompressedTopItems( artists = {} ,tracks = {} ){
+        const compressedArtists = [];
+        const compressedTracks = [];
+
+        for(let i = 0; Reflect.ownKeys(artists); i++){
+            compressedArtists.push(
+                {
+                    name: artists[i].name,
+                    image: artists[i].images?.at(-1)?.url,
+                    uri: artists[i].uri
+                }
+            )
+        }
+
+        for(let i =0; Reflect.ownKeys(tracks); i++){
+            compressedTracks.push(
+                {
+                    name: tracks[i].name,
+                    isLocal: tracks[i].is_local,
+                    album: tracks[i].album,
+                    artists: tracks[i].artists,
+                 }
+            )
+        }
+
+        return [compressedArtists , compressedTracks];
+    }
+
+
 }
 
 
